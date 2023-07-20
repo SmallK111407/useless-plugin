45cdeef333e2ff68af5244aa712f6ca0e7df58ee
SmallK111407 <3399280843@qq.com>
2023-07-21 05:37:38 +0800

    feat: 查询人物图片数量功能 Command:`#查询时雨数量`

---
+++ apps/queryNumber.js
@@ -0,0 +1,34 @@
+import plugin from '../../../lib/plugins/plugin.js'
+import fs from 'node:fs'
+import alias from '../model/alias.js'
+
+const _path = process.cwd() + '/plugins/useless-plugin'
+
+export class queryNumber extends plugin {
+    constructor() {
+        super({
+            name: '[无用插件]查询人物图片数量',
+            dsc: '查询人物图片数量',
+            event: 'message',
+            priority: 10,
+            rule: [
+                {
+                    reg: '^#*(无用)?(查询|查看|检查).*(图片|图)?(数量|张数|数)',
+                    fnc: 'queryNumber'
+                }
+            ]
+        })
+    }
+
+    async queryNumber() {
+        let msg = this.e.msg
+        let reg = msg.replace(/#|无用|查询|查看|图片|图|数量|张数|数/g, '').trim()
+        let name = reg
+        let role = alias.get(name)
+        if (!role) return false
+        const files = fs.readdirSync(`${_path}/goodjob-img/resources/${role}/`)
+        let number = Math.floor(files.length)
+        await this.reply(`当前所查询人物【${role}】\n总共拥有${number}张图片`)
+        return true
+    }
+}
