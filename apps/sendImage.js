26fbb6412a884f2a686aeb35ea40f2ef10002788
SmallK111407 <3399280843@qq.com>
2023-07-27 01:10:45 +0800

    新增 `#随机xx`对接云溪院API,条件为:当插件根目录下没有goodjob-img本地图库,则使用云溪院API返图

--- apps/sendImage.js
+++ apps/sendImage.js
@@ -26,9 +26,23 @@ export class sendImage extends plugin {
         let name = reg
         let role = alias.get(name)
         if (!role) return false
-        const files = fs.readdirSync(`${_path}/goodjob-img/resources/${role}/`)
-        let number = Math.floor(Math.random() * files.length)
-        await this.reply(segment.image(`${_path}/goodjob-img/resources/${role}/${files[number]}`))
-        return true
+        let result = fs.existsSync(`${_path}/goodjob-img`)
+        if (result === true) {
+            const files = fs.readdirSync(`${_path}/goodjob-img/resources/${role}/`)
+            let number = Math.floor(Math.random() * files.length)
+            await this.reply(segment.image(`${_path}/goodjob-img/resources/${role}/${files[number]}`))
+        } else {
+            console.log('[无用插件]未发现安装了本地图库，将尝试使用【云溪院API】返图')
+            let url = `https://yxy-api.yize.site/api/gaffe/index.php?list=${role}&type=json`
+            await fetch(url).catch((err) => logger.error(err))
+                .then(response =>
+                    response.json())
+                .then(data => {
+                    const imageUrl = data.image_url;
+                    console.log(data.image_url);
+                    this.e.reply(segment.image(imageUrl))
+                })
+            return true
+        }
     }
 }
