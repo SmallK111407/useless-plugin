9ebf76975bae3da086e8ef58d220ffc39e0ab0ed
SmallK111407 <3399280843@qq.com>
2023-07-27 22:39:31 +0800

    修改 戳一戳改为`list=sj`的接口返图

--- apps/pokeSend.js
+++ apps/pokeSend.js
@@ -35,24 +35,15 @@ export class poke extends plugin {
       } else {
         console.log('[无用插件]未发现安装了本地图库，将尝试使用【云溪院API】返图')
         // API from @云溪院
-        let url = `https://yxy-api.yize.site/api/gaffe/index.php?list=all`
+        let url = `https://yxy-api.yize.site/api/gaffe/?list=sj&type=json`
         await fetch(url).catch((err) => logger.error(err))
           .then(response =>
             response.json())
           .then(data => {
-            const randomIndex = Math.floor(Math.random() * data.length);
-            const randomData = data[randomIndex];
-            const url = `https://yxy-api.yize.site/api/gaffe/index.php?list=${randomData}&type=json`
-            console.log(randomData);
-            fetch(url).catch((err) => logger.error(err))
-              .then(response =>
-                response.json())
-              .then(data => {
-                const imageUrl = data.image_url;
-                console.log(data.image_url);
-                this.e.reply(segment.image(imageUrl))
-                return true
-              })
+            const imageUrl = data.image_url;
+            console.log(data.image_url);
+            this.e.reply(segment.image(imageUrl))
+            return true
           })
       }
     }
