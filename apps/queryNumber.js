d5807ea0c466ab2202f438640049ae4c8d5952bb
SmallK111407 <3399280843@qq.com>
2023-08-02 02:47:28 +0800

    新增 `#查询全部数量`

--- apps/queryNumber.js
+++ apps/queryNumber.js
@@ -9,14 +9,18 @@ const _path = process.cwd() + '/plugins/useless-plugin'
 export class queryNumber extends plugin {
     constructor() {
         super({
-            name: '[无用插件]查询人物图片数量',
-            dsc: '查询人物图片数量',
+            name: '[无用插件]查询图片数量',
+            dsc: '查询图片数量',
             event: 'message',
             priority: 10,
             rule: [
                 {
                     reg: '^#*(无用)?(查询|查看|检查)乐子(图片|图)?(数量|张数|数)',
                     fnc: 'queryHappyNumber'
+                },
+                {
+                    reg: '^#*(无用)?(查询|查看|检查)(全部|所有)(图片|图)?(数量|张数|数)',
+                    fnc: 'queryAll'
                 },
                 {
                     reg: '^#*(无用)?(查询|查看|检查).*(图片|图)?(数量|张数|数)',
@@ -32,6 +36,7 @@ export class queryNumber extends plugin {
             const files = fs.readdirSync(`${_path}/goodjob-img/resources/UNKNOWN/`)
             let number = Math.floor(files.length)
             await this.reply(`当前所查询「乐子」\n目前总共已收录${number}张图片`, true)
+            return true
         } else {
             console.log('[无用插件]未发现安装了本地图库，将尝试使用【云溪院API】查询')
             // API from @云溪院
@@ -44,6 +49,32 @@ export class queryNumber extends plugin {
                     console.log(data.image_count);
                     this.e.reply(`当前所查询「乐子」\n目前总共已收录${imageCount}张图片`, true)
                 })
+            return true
+        }
+    }
+    async queryAll() {
+        let result = fs.existsSync(`${_path}/goodjob-img`)
+        if (result === true) {
+            let count = 0;
+            function countImages(folder) {
+                const files = fs.readdirSync(folder);
+                for (const file of files) {
+                    const path = folder + '/' + file;
+                    if (fs.statSync(path).isDirectory()) {
+                        countImages(path);
+                    } else if (/\.(jpe?g|png|gif)$/i.test(file)) {
+                        count++;
+                    }
+                }
+            }
+            countImages(`${_path}/goodjob-img`)
+            this.e.reply(`目前本图库总共已收录${count}张图片`, true)
+            return true
+        } else {
+            // API from @云溪院
+            //let url = `https://yxy-api.yize.site/api/gaffe/index.php?`
+            this.e.reply(`请安装无用图库，否则无法查询全部图片数量，或等待后续API适配`, true)
+            return true
         }
     }
     async queryNumber() {
@@ -57,6 +88,7 @@ export class queryNumber extends plugin {
             const files = fs.readdirSync(`${_path}/goodjob-img/resources/${role}/`)
             let number = Math.floor(files.length)
             await this.reply(`当前所查询人物「${role}」\n目前总共已收录${number}张图片`, true)
+            return true
         } else {
             console.log('[无用插件]未发现安装了本地图库，将尝试使用【云溪院API】查询')
             // API from @云溪院
@@ -69,7 +101,7 @@ export class queryNumber extends plugin {
                     console.log(data.image_count);
                     this.e.reply(`当前所查询人物「${role}」\n目前总共已收录${imageCount}张图片`, true)
                 })
+            return true
         }
-        return true
     }
 }
