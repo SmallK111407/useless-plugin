0e9c598c59811ee9bf00bcabc59cee9ce04da947
SmallK111407 <3399280843@qq.com>
2023-08-12 20:41:46 +0800

    修复 查看乐子第x张图

--- apps/checkImage.js
+++ apps/checkImage.js
@@ -14,11 +14,53 @@ export class checkImage extends plugin {
             priority: 10,
             rule: [
                 {
+                    reg: '^#*(无用)?(查看|发送)乐子(第)?.*(张|号)?(图片|照片|图)?',
+                    fnc: 'checkHappyImage'
+                },
+                {
                     reg: '^#*(无用)?(查看|发送).*(第)?.*(张|号)?(图片|照片|图)?',
                     fnc: 'checkImage'
                 }
             ]
         })
+    }
+    async checkHappyImage() {
+        let msg = this.e.msg
+        let value = msg.replace(/[^0-9]/ig, "");
+        let reg = msg.replace(/#|无用|查看|发送|第|[0-9]|张|号|图片|照片|图/g, '').trim()
+        let name = reg
+        let number = Number(value) - 1
+        let test = fs.existsSync(`${_path}/goodjob-img`)
+        if (test === false) {
+            console.log('[无用插件]未发现安装了本地图库，将尝试使用【云溪院API】返图')
+            // API from @云溪院
+            await fetch(`https://yxy-api.yize.site/api/gaffe/goodjob-img/resources/${name}/${number}.gif`)
+                .then(response => {
+                    if (!response.ok) {
+                        console.log('[无用插件]云溪院API 404 Error');
+                        this.e.reply(`当前所查看图片不存在或是API访问出现问题！\n你可以使用【#查询乐子数量】来查看乐子图片数量`, true)
+                        return true
+                    } else {
+                        console.log('[无用插件]云溪院API访问成功!');
+                        this.e.reply(segment.image(`https://yxy-api.yize.site/api/gaffe/goodjob-img/resources/${name}/${number}.png`))
+                        return true
+                    }
+                })
+                .catch(error => {
+                    console.log('Error:', error)
+
+                })
+        } else {
+            const files = fs.readdirSync(`${_path}/goodjob-img/resources/${name}/`)
+            let result = fs.existsSync(`${_path}/goodjob-img/resources/${name}/${files[number]}`)
+            if (result === true) {
+                await this.reply(segment.image(`${_path}/goodjob-img/resources/${name}/${files[number]}`))
+                return true
+            } else if (result === false) {
+                await this.reply(`当前所查看图片不存在！\n你可以使用【#查询乐子数量】来查看乐子图片数量`, true)
+                return true
+            }
+        }
     }
     async checkImage() {
         let msg = this.e.msg
