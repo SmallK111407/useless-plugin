9cd18c301bd3b7e30bf4b1f5159385c12bfc78a3
SmallK111407 <3399280843@qq.com>
2023-08-19 17:38:34 +0800

    6

--- model/alias.js
+++ model/alias.js
@@ -2,12 +2,13 @@ import setting from './setting.js'
 import _ from 'lodash'
 import fs from 'node:fs'
 
+let defAlias = null
 async function loadAliasData() {
   try {
-    const jsonContent = await fs.readFile('./aliasData/alias.json', 'utf-8');
-    defAlias = JSON.parse(jsonContent);
+    const jsonContent = await fs.readFile('./aliasData/alias.json', 'utf-8')
+    defAlias = JSON.parse(jsonContent)
   } catch (error) {
-    console.error('加载原配置文件错误:', error);
+    console.error('加载原配置文件错误:', error)
   }
 }
 
