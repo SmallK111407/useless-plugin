022d3eaee1c9d42506408e2230d43889470a2e84
SmallK111407 <3399280843@qq.com>
2023-08-23 23:34:32 +0800

    s

--- model/path.js
+++ model/path.js
@@ -7,7 +7,7 @@ const pluginName = path.basename(path.join(import.meta.url, '../../'))
 // 插件根目录
 const pluginRoot = path.join(_path, 'plugins', pluginName)
 // 插件资源目录
-const pluginResources = path.join(pluginRoot, 'resource')
+const pluginResources = path.join(pluginRoot, 'resources')
 
 export {
   _path,
