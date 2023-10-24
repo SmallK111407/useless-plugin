1af89092055b239dab37fb0781bd39b9b5de4747
SmallK111407 <3399280843@qq.com>
2023-10-25 00:38:28 +0800

    feat: #全部<人物>

---
+++ apps/sendAllImages.js
@@ -0,0 +1,48 @@
+import plugin from '../../../lib/plugins/plugin.js'
+import common from '../../../lib/common/common.js'
+import alias from '../model/alias.js'
+import fs from 'node:fs'
+import path from 'path'
+
+const _path = process.cwd() + '/plugins/useless-plugin'
+
+export class sendAllImages extends plugin {
+    constructor() {
+        super({
+            name: '[无用插件]查看人物全部图片',
+            dsc: '查看单独一个人物的所有的图片',
+            event: 'message',
+            priority: 11,
+            rule: [
+                {
+                    reg: '^#*(无用)?(查看|发送)?(全部|所有).*',
+                    fnc: 'sendAllImages'
+                }
+            ]
+        })
+    }
+    async sendAllImages() {
+        let reg = this.e.msg.replace(/#|无用|查看|发送|全部|所有/g, '').trim()
+        let name = reg
+        let role = alias.get(name)
+        if (!role) return false
+        try {
+            let msg = []
+            const dirPath = `${_path}/goodjob-img/resources/${role}`
+            const files = await fs.readdirSync(dirPath);
+            for (let i = 0; i < files.length; i++) {
+                let file = files[i];
+                let filePath = path.join(dirPath, file);
+                let isImage = ['.jpeg', '.jpg', '.gif', '.png'].some(i => filePath.endsWith(i));
+                if (isImage) {
+                    msg.push([`${i + 1}`, segment.image(filePath)]);
+                }
+            }
+            let reply = await this.e.reply(await common.makeForwardMsg(this.e, msg, `点击查看全部「${role}」，共${files.length}张`))
+            if (!reply) this.e.reply(`发送失败...可能是图片过多，可尝试私聊查看`, true)
+        } catch (error) {
+            logger.error(error)
+        }
+        return true
+    }
+}
