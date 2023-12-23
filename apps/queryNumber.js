0bf8f5702e3894ea7d3c654e40bfd9a1569a7792
小花花花儿 <860563585bxy@gmail.com>
2023-12-23 16:31:22 +0000

    update apps/queryNumber.js.
    Signed-off-by: 小花花花儿 <860563585bxy@gmail.com>

--- apps/queryNumber.js
+++ apps/queryNumber.js
@@ -74,7 +74,7 @@ export class queryNumber extends plugin {
             return true
         } else {
             logger.debug('[无用插件]未发现安装了本地图库，将尝试使用【云溪院API】查询')
-            let url = `https://yunxiyuan.xyz/api/gaffe/index.php?list=num`
+            let url = `https://api.yunxiyuanyxy.xyz/gaffe/index.php?list=num`
             await fetch(url, {
                 headers: {
                     'Accept': 'application/json',
